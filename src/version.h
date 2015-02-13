36d4141c001fab49879491706cb764bcdf7a92b9
upgradeadvice <upgradeadvice@gmail.com>
2015-02-12 22:34:48 -0600

    Repair version.h
    Repair version.h

--- src/version.h
+++ src/version.h
@@ -15,7 +15,7 @@ static const int CLIENT_VERSION =
                            1000000 * CLIENT_VERSION_MAJOR
                          +   10000 * CLIENT_VERSION_MINOR
                          +     100 * CLIENT_VERSION_REVISION
-                         +       1 * CLIENT_VERSION_BUILD;
+                         +       3 * CLIENT_VERSION_BUILD;
 
 extern const std::string CLIENT_NAME;
 extern const std::string CLIENT_BUILD;
@@ -28,20 +28,20 @@ extern const std::string CLIENT_DATE;
 static const int PROTOCOL_VERSION = 70003;
 
 // earlier versions not supported and are disconnected
-static const int MIN_PROTO_VERSION = 70000;
+static const int MIN_PROTO_VERSION = 209;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
-static const int CADDR_TIME_VERSION = 70000;
+static const int CADDR_TIME_VERSION = 31402;
 
 // only request blocks from nodes outside this range of versions
-static const int NOBLKS_VERSION_START = 70000;
-static const int NOBLKS_VERSION_END = 70003;
+static const int NOBLKS_VERSION_START = 32000;
+static const int NOBLKS_VERSION_END = 70000;
 
 // BIP 0031, pong message, is enabled for all versions AFTER this one
-static const int BIP0031_VERSION = 70000;
+static const int BIP0031_VERSION = 60000;
 
 // "mempool" command, enhanced "getdata" behavior starts with this version:
-static const int MEMPOOL_GD_VERSION = 70000;
+static const int MEMPOOL_GD_VERSION = 60002;
 
 #endif
