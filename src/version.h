31f8bb7f2e6f4287f053df08ea68f3700f7f62f7
upgradeadvice <upgradeadvice@gmail.com>
2015-02-12 19:57:54 -0600

    Bump protocol to 70003
    Bump protocol to 70003

--- src/version.h
+++ src/version.h
@@ -25,23 +25,23 @@ extern const std::string CLIENT_DATE;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 70002;
+static const int PROTOCOL_VERSION = 70003;
 
-// earlier versions not supported as of Feb 2012, and are disconnected
-static const int MIN_PROTO_VERSION = 209;
+// earlier versions not supported and are disconnected
+static const int MIN_PROTO_VERSION = 70000;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
-static const int CADDR_TIME_VERSION = 31402;
+static const int CADDR_TIME_VERSION = 70000;
 
 // only request blocks from nodes outside this range of versions
-static const int NOBLKS_VERSION_START = 32000;
-static const int NOBLKS_VERSION_END = 70000;
+static const int NOBLKS_VERSION_START = 70000;
+static const int NOBLKS_VERSION_END = 70003;
 
 // BIP 0031, pong message, is enabled for all versions AFTER this one
-static const int BIP0031_VERSION = 60000;
+static const int BIP0031_VERSION = 70000;
 
 // "mempool" command, enhanced "getdata" behavior starts with this version:
-static const int MEMPOOL_GD_VERSION = 60002;
+static const int MEMPOOL_GD_VERSION = 70000;
 
 #endif
