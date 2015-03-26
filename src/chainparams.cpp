011313c74e9fa9cf19cb6f7b5e90aac269814b82
upgradeadvice <upgradeadvice@gmail.com>
2015-03-26 06:36:43 -0500

    Update seeds

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -70,10 +70,11 @@ public:
         assert(hashGenesisBlock == hashMainGenesisBlock);
         assert(genesis.hashMerkleRoot == uint256("0xa2cc3c1d8ab6e50e80464693199eefdd172d45c4129998394e636e47b5621364"));
 
-        vSeeds.push_back(CDNSSeedData("104.131.125.97", "104.131.125.97"));
-        vSeeds.push_back(CDNSSeedData("103.19.252.82", "103.19.252.82"));
-        vSeeds.push_back(CDNSSeedData("178.62.247.76", "178.62.247.76"));
-        vSeeds.push_back(CDNSSeedData("104.131.66.88", "104.131.66.88"));
+        vSeeds.push_back(CDNSSeedData("dnsseed.monetaryunit.tk", "dnsseed.monetaryunit.tk"));
+        vSeeds.push_back(CDNSSeedData("dnsseed2.monetaryunit.tk", "dnsseed2.monetaryunit.tk"));
+        // vSeeds.push_back(CDNSSeedData("103.19.252.82", "103.19.252.82"));
+        // vSeeds.push_back(CDNSSeedData("178.62.247.76", "178.62.247.76"));
+        // vSeeds.push_back(CDNSSeedData("104.131.66.88", "104.131.66.88"));
 
         base58Prefixes[PUBKEY_ADDRESS] = list_of(15);
         base58Prefixes[SCRIPT_ADDRESS] = list_of(9);
