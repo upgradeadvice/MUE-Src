d35f9e151ccf60dcc1b47d62ab72bfd09f6d0fa9
upgradeadvice <upgradeadvice@gmail.com>
2015-04-11 19:46:03 -0500

    1001 release

--- src/version.cpp
+++ src/version.cpp
@@ -13,7 +13,7 @@
 const std::string CLIENT_NAME("Monetaryunit");
 
 // Client version number
-#define CLIENT_VERSION_SUFFIX   "-beta"
+#define CLIENT_VERSION_SUFFIX   ""
 
 
 // The following part of the code determines the CLIENT_BUILD variable.
@@ -48,7 +48,7 @@ const std::string CLIENT_NAME("Monetaryunit");
     "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "." DO_STRINGIZE(build) "-g" commit
 
 #define BUILD_DESC_FROM_UNKNOWN(maj,min,rev,build) \
-    "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "." DO_STRINGIZE(build) "-unk"
+    "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "." DO_STRINGIZE(build) ""
 
 #ifndef BUILD_DESC
 #    ifdef BUILD_SUFFIX
